e8fb467ac700d261fef8e299edc837f564fefca2
Ruslan865 <babashov666@gmail.com>
2025-10-15 07:25:09 -0700

    Update vite.config.js
    Signed-off-by: Ruslan865 <babashov666@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/my-web3-platform/',
-})
+  base: "/my-web3-platform/",
+  plugins: [react()],
+});
